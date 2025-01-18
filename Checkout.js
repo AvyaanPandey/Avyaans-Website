4cc3beba86d6375f99e57c86408f6aa0754cd4c9
AvyaanPandey <avyaanp09@gmail.com>
2025-01-18 18:57:57 +0000

    I added stuff to Checkout.js and Checkout.html!!!

--- Checkout.js
+++ Checkout.js
@@ -1,10 +1,22 @@
 
 let cart = JSON.parse(localStorage.getItem("cart"))
 console.log(cart)
+let payment_body = document.getElementById("payment_body")
  for(let i = 0; i < cart.length; i++){
    let item = document.createElement("div")
-    item.append(cart[i])
-   let payment_body = document.getElementsByClassName("payment_body")
-   payment_body.append(item)
+    item.textContent = cart[i]
+   
+   payment_body.appendChild(item)
  }
 console.log(cart)
+
+
+// function removeItem(item){
+//    let index = cart.indexOf(item)
+
+//    if(index > -1){
+//       cart.splice(index, 1)
+//    }
+
+//    localStorage.setItem('cart', JSON.stringify(cart))
+// }
